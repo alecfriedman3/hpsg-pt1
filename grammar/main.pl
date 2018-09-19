8092f3a601b88f4fa1c208540b32dce3f7bacf7f
Alec Friedman <alec.friedman3@gmail.com>
2018-09-19 13:18:04 -0400

    init

---
+++ grammar/main.pl
@@ -0,0 +1,29 @@
+
+% ---------------   Load ALE  ----------------
+
+% ale must be located one directory up 
+init :- 
+   compile('../ale_swi.pl'), 
+   ldg.
+
+ldg :- 
+  compile_gram('grammar.pl').
+  %  compile_sig('signature'),
+  %  compile_fun('macros'),
+  %  compile_cons('cons'),
+  %  compile_logic,
+  %  compile_subsume,
+  %  compile_dcs('if_rules'),
+  %  compile_lex_rules('lex_rules'),
+  %  compile_lex('lexicon'),
+  %  compile_rules('phrase_rules').
+
+
+% Grammar consistency check
+
+p :- 
+   time( rec_list([
+                           [she,walks], 
+                           [she,walked]
+                         ],
+                           phrase, _) ).
